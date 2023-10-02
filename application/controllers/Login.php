b83b6927883d86ccf47e9d97cfc29c99f7409a4c
Stain Francis <educaysoft@gmail.com>
2023-10-02 06:51:01 -0400

    mejoras

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -27,6 +27,7 @@ public function index() {
 	
 //	 $data['eventos']= $this->evento_model->lista_eventos_open(0)->result();
 //	 $this->load->view('template/page_header.php');
+	 $data['eventos']="";
 	 $this->load->view('login_form',$data);
 //	 $this->load->view('template/page_footer.php');
 }
