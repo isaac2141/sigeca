704497ec3313fd85dadaa561ecbcbe2436bc2eb5
ElProfe <educaysoft@gmail.com>
2023-10-17 14:34:32 -0500

    MEJORAS

---
+++ application/controllers/Garciadilan.php
@@ -0,0 +1,15 @@
+<?php
+class Garciadilan extends CI_Controller{
+public function __construct()
+{
+    parent::__construct();
+}
+
+public function index()
+{
+    $data['bienvenida']="Bienvenido soy Dilan Garcia estoy en la Ingenieria de Software";
+    $this->load->view('garciadilan_record',$data);
+}
+}
+
+?>
---
+++ application/views/garciadilan_record.php
@@ -0,0 +1,8 @@
+<?php
+
+
+echo "<b>Hola Dilan Grancia</b>, sica ha vueldo a la vida como SIGECA<br>";
+echo $bienvenida;
+
+
+?>
