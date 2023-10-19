bb5a8e03e30e895ac6186d419091222eb85abbac
Stalin Francis <educaysoft@gmail.com>
2023-10-19 12:00:29 -0500

    mejoras

--- application/config/config.php
+++ application/config/config.php
@@ -34,7 +34,7 @@ $config['base_url'] = "https://".$_SERVER['SERVER_NAME'].'/sigeca';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
