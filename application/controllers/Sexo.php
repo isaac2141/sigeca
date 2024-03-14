216ad418eb9edadb63adab5081b2c6f92052e22b
Stalin Francis <educaysoft@gmail.com>
2024-03-13 20:08:11 -0500

    mejoras

--- application/controllers/Sexo.php
+++ application/controllers/Sexo.php
@@ -11,7 +11,9 @@ public function index(){
   	$data['sexo']=$this->sexo_model->sexo(1)->row_array();
  
   	$data['title']="Tipos de documentos";
+	$this->load->view('template/page_header');
   	$this->load->view('sexo_record',$data);
+	$this->load->view('template/page_footer');
 }
 
 
