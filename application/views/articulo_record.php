8bafc66d1aa9e57eff9c46897975d95a8ab402e3
DoomerCami <camily.muso.diaz@utelvt.edu.ec>
2024-05-04 16:00:34 -0500

    modificacion del grupo SofLab

--- application/views/articulo_record.php
+++ application/views/articulo_record.php
@@ -32,7 +32,7 @@ if(isset($articulo))
 
 
  <div class="form-group row">
-    <label class="col-md-2 col-form-label"> Id artículo:</label>
+    <label class="col-md-2 col-form-label"> Id:</label>
 	<div class="col-md-10">
      <?php
 
