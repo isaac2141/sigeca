7e0d88b64b9529852ce2668c786624dadf2e2c78
josemm0505 <mejiamicoltalol@gmail.com>
2024-05-04 15:40:06 -0500

    Cambios hechos por el equipo SoftWork

--- application/views/genero_form.php
+++ application/views/genero_form.php
@@ -1,22 +1,19 @@
 <h2> <?php echo $title; ?> </h2>
 <hr/>
 <?php echo form_open("genero/save") ?>
-<?php echo form_hidden("idgenero")  ?>
-<table>
+<?php echo form_hidden("idgenero")  ?> 
 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Nombre:</label>
+	<div class="col-md-10">
+<?php echo form_input("nombre","", array("placeholder"=>"Descripcion de Género: "));  ?>
 
+	</div> 
+</div> 
 
-
-
-<tr>
-<td> Nombre </td>
-<td><?php echo form_input("nombre","", array("placeholder"=>"Genero"))  ?></td>
-</tr>
-
-<tr>
-<td colspan="2"> <hr><?php echo form_submit("submit", "Guardar"); ?><?php echo anchor("genero","Atras") ?> </td>
-</tr>
-
-</table>
+<div class="form-group row">
+	<div class="col-md-10">
+    <hr><?php echo form_submit("submit", "Guardar"); ?><?php echo anchor("genero","Atras") ?>
+</div>
 <?php echo form_close();?>
 
