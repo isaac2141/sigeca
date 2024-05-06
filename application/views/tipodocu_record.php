86163430343f939469cd3919b501bf8f4bb8065e
Héctor-Lozada <eeaae7570@gmail.com>
2024-05-06 18:23:18 -0500

    mejoras

--- application/views/tipodocu_record.php
+++ application/views/tipodocu_record.php
@@ -18,7 +18,7 @@
 
 
 <?php echo form_hidden('idtipodocu',$tipodocu['idtipodocu']) ?>
-<div class=' form-group row'>
+<div class='form-group row'>
   <label class='col-md-2 col-form-label'>Id:</label>
   <div class='col-md-10'>
     <?php echo form_input('idtipodocu',$tipodocu['idtipodocu'],array("disabled"=>"disabled",'placeholder'=>'Idtipodocus')) ?>
