c098614c910aae97e76c192022a7bbfdaf3af647
Héctor-Lozada <eeaae7570@gmail.com>
2024-05-07 17:14:01 -0500

    Mejoras en edit de tipo docu

--- application/views/tipodocu_edit.php
+++ application/views/tipodocu_edit.php
@@ -5,7 +5,7 @@
 <div class='form-group row'>
     <label class='col-md-2 col-form-label'>Id:</label>
     <div class='col-md-10'>
-      <?php echo form_textarea('idtipodocu',$tipodocu['idtipodocu'],array('placeholder'=>'Idtipodocu')) ?>
+      <?php echo form_input('idtipodocu',$tipodocu['idtipodocu'],array('placeholder'=>'Idtipodocu')) ?>
     </div>
 </div>
 <div class="form-group row">
